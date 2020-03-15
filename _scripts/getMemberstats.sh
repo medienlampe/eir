e893382dead6e309639f999b87900bfd8dd22378
Sven Bischoff <sven@medienlampe.de>
2020-03-15 16:43:10 +0100

    Fixes script.

--- _scripts/getMemberstats.sh
+++ _scripts/getMemberstats.sh
@@ -8,7 +8,7 @@ bzip2 -dk daily_user_summary.txt.bz2
 rm -f donorlist.txt
 touch donorlist.txt
 head -n +2 daily_user_summary.txt | tail -n1 >> donorlist.txt
-grep -P "$TEAMID\+$" daily_user_summary.txt >> donorlist.txt
+grep -e "$TEAMID\+$" daily_user_summary.txt >> donorlist.txt
 sed 's/\t/,/g' < donorlist.txt > donorlist.csv
 any-json donorlist.csv > $TARGETFOLDER/donorlist.json
 rm -rf donorlist.txt donorlist.csv daily_user_summary.*
