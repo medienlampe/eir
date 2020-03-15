02a8192cb07bb4f12ae806d88bc582c791eb290f
Sven Bischoff <sven@medienlampe.de>
2020-03-15 16:28:01 +0100

    Adjusts update scripts.

--- _scripts/getMemberstats.sh
+++ _scripts/getMemberstats.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 TEAMID=237997
+TARGETFOLDER=/root/eir/_data
 
 wget https://apps.foldingathome.org/daily_user_summary.txt.bz2
 bzip2 -dk daily_user_summary.txt.bz2
@@ -9,5 +10,5 @@ touch donorlist.txt
 head -n +2 daily_user_summary.txt | tail -n1 >> donorlist.txt
 grep -P "$TEAMID\+$" daily_user_summary.txt >> donorlist.txt
 sed 's/\t/,/g' < donorlist.txt > donorlist.csv
-any-json donorlist.csv > ../_data/donorlist.json
+any-json donorlist.csv > $TARGETFOLDER/donorlist.json
 rm -rf donorlist.txt donorlist.csv daily_user_summary.*
--- _scripts/getTeamstats.sh
+++ _scripts/getTeamstats.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TEAMID=237997
 TEAMNAME=Invia
+TARGETFOLDER=/root/eir/_data
 
 wget https://apps.foldingathome.org/daily_team_summary.txt.bz2
 bzip2 -dk daily_team_summary.txt.bz2
@@ -10,5 +11,5 @@ touch teamscore.txt
 head -n +2 daily_team_summary.txt | tail -n1 >> teamscore.txt
 grep -P "$TEAMID\t$TEAMNAME" daily_team_summary.txt >> teamscore.txt
 sed 's/\t/,/g' < teamscore.txt > teamscore.csv
-any-json teamscore.csv > ../_data/teamscore.json
+any-json teamscore.csv > $TARGETFOLDER/teamscore.json
 rm -rf teamscore.txt teamscore.csv daily_team_summary.*
