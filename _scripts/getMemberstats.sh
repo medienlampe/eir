d97419fe4d633dc4e66d2eb7c890114da02a4a6b
Sven Bischoff <sven@medienlampe.de>
2020-03-25 16:30:25 +0100

    Fixes script for usage in crontab.

--- _scripts/getMemberstats.sh
+++ _scripts/getMemberstats.sh
@@ -10,5 +10,5 @@ touch donorlist.txt
 head -n +2 daily_user_summary.txt | tail -n1 >> donorlist.txt
 grep -e "$TEAMID\+$" daily_user_summary.txt >> donorlist.txt
 sed 's/\t/,/g' < donorlist.txt > donorlist.csv
-any-json donorlist.csv > $TARGETFOLDER/donorlist.json
+/usr/local/bin/any-json donorlist.csv > $TARGETFOLDER/donorlist.json
 rm -rf donorlist.txt donorlist.csv daily_user_summary.*
--- _scripts/getTeamstats.sh
+++ _scripts/getTeamstats.sh
@@ -11,5 +11,5 @@ touch teamscore.txt
 head -n +2 daily_team_summary.txt | tail -n1 >> teamscore.txt
 grep -P "$TEAMID\t$TEAMNAME" daily_team_summary.txt >> teamscore.txt
 sed 's/\t/,/g' < teamscore.txt > teamscore.csv
-any-json teamscore.csv > $TARGETFOLDER/teamscore.json
+/usr/local/bin/any-json teamscore.csv > $TARGETFOLDER/teamscore.json
 rm -rf teamscore.txt teamscore.csv daily_team_summary.*
